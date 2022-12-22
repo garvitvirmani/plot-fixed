d901ebec6769065a496fa3d7064bcaa6798c35cb
garvitvirmani <garvitvirmani@gmail.com>
2022-12-22 20:03:48 +0530

    plot-fixed

---
+++ App.js
@@ -0,0 +1,23 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { MovieContext } from "./Context";
+import HomeScreen from "./screens/HomeScreen";
+import StackNavigator from "./StackNavigator";
+
+export default function App() {
+  return (
+    <>
+      <MovieContext>
+        <StackNavigator />
+        <StatusBar style="auto" />
+      </MovieContext>
+    </>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+  },
+});
